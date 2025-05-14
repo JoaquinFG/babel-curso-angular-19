fef05cb7294ea2afcc23b1168a254e0bff30e1bc
Joaquin Ferreira Garcia <jferreig@ext.unicaja.es>
2025-05-14 18:11:48 +0200

    random beer

--- src/app/pages/beer-random/beer-random.component.ts
+++ src/app/pages/beer-random/beer-random.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
+import { BeerService } from '../../services/beer.service';
+import { Beer } from '../../models/beer.model';
 
 @Component({
   selector: 'app-beer-random',
   imports: [],
   templateUrl: './beer-random.component.html',
-  styleUrl: './beer-random.component.scss'
+  styleUrl: './beer-random.component.scss',
 })
-export class BeerRandomComponent {
+export class BeerRandomComponent implements OnInit {
+
+  private readonly _beerService = inject(BeerService);
+
+  randomBeer: Beer | undefined;
+  beerImage: string | undefined;
+
+  constructor(){
+  }
+  
+  ngOnInit(): void {
+    this._beerService.getRandomBeer().subscribe((data) => {
+      this.randomBeer = data;
+      this.beerImage = `https://punkapi.online/v3/images/${data.image}`
+    });
+  }
 
 }
